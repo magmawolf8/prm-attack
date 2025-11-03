0aaf3d70c54b4b5c4f1c6560ba32f073ec341216
Heng Yang <git@asherew.com>
2025-11-03 13:12:52 -0800

    new branch + continuous + discrete

---
+++ config.py
@@ -0,0 +1,16 @@
+SKYWORK_MODEL_NAME = "Skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B"
+STEP_TOKEN = "\n\n"
+
+# General hyperparameters
+RANDOM_SEED = 420
+NUM_PREFIXES = 1
+
+# Training time hyperparameters
+NUM_EPOCHS = 3
+DATA_SUBSET_SIZE = 2000
+BATCH_SIZE = 1
+
+# Optimizer behavior
+LEARNING_RATE = 1e-2
+L1_LAMBDA = 0.0
+MOMENTUM=0.0
--- test/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=7 python3 test.py --jsonl phase2_train.jsonl --vllm-base-url http://localhost:12341/v1 --vllm-api-key EMPTY
